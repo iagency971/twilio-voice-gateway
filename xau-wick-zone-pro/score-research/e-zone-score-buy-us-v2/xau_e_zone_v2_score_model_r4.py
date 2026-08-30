52437224ed0187590c9131c3b7c4cee5598f3e24
iagency971 <iagency971@gmail.com>
2026-08-30 10:52:58 -0400

    add R4 outcome authorization wrapper for score model

---
+++ xau-wick-zone-pro/score-research/e-zone-score-buy-us-v2/xau_e_zone_v2_score_model_r4.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+from __future__ import annotations
+import xau_e_zone_v2_score_model as base
+
+base.TOKEN='GO_E_ZONE_SCORE_BUY_US_V2_R4_SEQUENTIAL_HISTORICAL_EXECUTION'
+
+if __name__=='__main__':
+    base.main()
